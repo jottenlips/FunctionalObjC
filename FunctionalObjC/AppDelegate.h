bd0745aa9a846e4a9669d89a2cdc2ead3194dbc0
johno <johno@aloompa.com>
2018-08-29 09:20:47 -0500

    Initial Commit

---
+++ FunctionalObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunctionalObjC
+//
+//  Created by John on 8/29/18.
+//  Copyright © 2018 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunctionalObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunctionalObjC
+//
+//  Created by John on 8/29/18.
+//  Copyright © 2018 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
